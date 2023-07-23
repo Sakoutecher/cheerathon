9bb20e3ad8c18d087ae2858efe7c3d9ff956a671
hcampos <hcampospro@gmail.com>
2023-07-23 16:32:20 +0200

    refactor(main.ts): rename variables hours, minutes, seconds to hoursShow, minutesShow, secondsShow for better clarity and semantics feat(main.ts): add countdown timer functionality to display hours, minutes, and seconds and update them every second feat(main.ts): add logic to stop the timer when hours, minutes, and seconds reach zero

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,32 @@
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
+const hoursShow = document.querySelector('.hours');
+const minutesShow = document.querySelector('.minutes');
+const secondsShow = document.querySelector('.seconds');
 const notificationMessage = document.querySelector('.notification-message');
 
-console.log(hours, minutes, seconds, notificationMessage);
+let hours = 0;
+let minutes = 0;
+let seconds = 5;
+
+const timer = () => {
+  seconds--;
+  if (seconds < 0) {
+    seconds = 59;
+    minutes--;
+  }
+  if (minutes < 0) {
+    minutes = 59;
+    hours--;
+  }
+}
+
+const showTime = () => {
+  timer();
+  hoursShow.innerHTML = hours.toString();
+  minutesShow.innerHTML = minutes.toString();
+  secondsShow.innerHTML = seconds.toString();
+  if ((hours <= 0) && (minutes <= 0) && (seconds <= 0)) {
+    clearInterval(intervalId);
+  }
+}
+
+let intervalId = setInterval(showTime, 1000);
