a50ec23ca7b276d9e966ff66aba4c3ab350a98dd
hcampos <hcampospro@gmail.com>
2023-07-23 16:19:05 +0200

    chore(main.ts): add query selectors for hours, minutes, seconds, and notificationMessage to improve code readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log('Hello World')
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const notificationMessage = document.querySelector('.notification-message');
+
+console.log(hours, minutes, seconds, notificationMessage);
