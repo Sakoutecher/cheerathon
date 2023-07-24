b00c630c01630bf3af0facec10ada3823d484524
hcampos <hcampospro@gmail.com>
2023-07-24 10:43:46 +0200

    fix(main.ts): change initial values of hours, minutes, and seconds variables to strings to match their usage in the code refactor(main.ts): extract addZero function to format single digit values with leading zero refactor(main.ts): simplify timer function logic by converting variables to strings and using parseInt and toString methods refactor(main.ts): update showTime function to display formatted hours, minutes, and seconds values refactor(main.ts): update condition in showTime function to use parseInt for comparison

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,43 @@ const minutesShow = document.querySelector('.minutes');
 const secondsShow = document.querySelector('.seconds');
 const notificationMessage = document.querySelector('.notification-message');
 
-let hours = 0;
-let minutes = 0;
-let seconds = 5;
+let hours = '15';
+let minutes = '23';
+let seconds = '45';
+
+const addZero = (value: number | string) => {
+  if (typeof value === 'number') {
+    value = value.toString(); // Convertit le nombre en chaîne de caractères
+  }
+  if (value.length === 1) {
+    return `0${value}`;
+  }
+  return value;
+};
 
 const timer = () => {
-  seconds--;
-  if (seconds < 0) {
-    seconds = 59;
-    minutes--;
+  seconds = (parseInt(seconds) - 1).toString();
+  if (parseInt(seconds) < 0) {
+    seconds = '59';
+    minutes = (parseInt(minutes) - 1).toString();
   }
-  if (minutes < 0) {
-    minutes = 59;
-    hours--;
+  if (parseInt(minutes) < 0) {
+    minutes = '59';
+    hours = (parseInt(hours) - 1).toString();
   }
-}
+  hours = addZero(hours);
+  minutes = addZero(minutes);
+  seconds = addZero(seconds);
+};
 
 const showTime = () => {
   timer();
-  hoursShow.innerHTML = hours.toString();
-  minutesShow.innerHTML = minutes.toString();
-  secondsShow.innerHTML = seconds.toString();
-  if ((hours <= 0) && (minutes <= 0) && (seconds <= 0)) {
+  hoursShow.innerHTML = hours;
+  minutesShow.innerHTML = minutes;
+  secondsShow.innerHTML = seconds;
+  if (parseInt(hours) <= 0 && parseInt(minutes) <= 0 && parseInt(seconds) <= 0) {
     clearInterval(intervalId);
   }
-}
+};
 
 let intervalId = setInterval(showTime, 1000);
