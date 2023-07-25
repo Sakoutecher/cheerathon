c87ce320d1a3d1f6d4775ca82371393f1009a596
hcampos <hcampospro@gmail.com>
2023-07-25 12:23:01 +0200

    fix(main.ts): convert localStorage values to strings to ensure consistent data type feat(main.ts): add function to calculate bits based on given time and multiplier feat(main.ts): add support for displaying notifications and updating time based on bits received from tmi.js feat(main.ts): add function to remove leading zero from input string feat(main.ts): add event listener for 'cheer' event from tmi.js to update time and display notification based on bits received

--- src/main.ts
+++ src/main.ts
@@ -7,44 +7,50 @@ const notification = document.querySelector('#notification') as HTMLElement;
 let hours = '';
 let minutes = '';
 let seconds = '';
+const tempsInitial = '00:00:06';
 
 // Récupère les valeurs dans le localStorage
 if (localStorage.getItem('hours') === null) {
   hours = '23';
 } else {
-  hours = localStorage.getItem('hours');
+  hours = localStorage.getItem('hours').toString();
 }
 if (localStorage.getItem('minutes') === null) {
   minutes = '59';
 } else {
-  minutes = localStorage.getItem('minutes');
+  minutes = localStorage.getItem('minutes').toString();
 }
 if (localStorage.getItem('seconds') === null) {
   seconds = '59';
 } else {
-  seconds = localStorage.getItem('seconds');
+  seconds = localStorage.getItem('seconds').toString();
 }
 
-import tmi from 'tmi.js';
+const calcBits = (temps, multiplicateur) => {
+  // Analyser le temps donné au format "hh:mm:ss"
+  const [heures, minutes, secondes] = temps.split(':').map(Number);
 
-const client = new tmi.Client({
-	channels: [ 'ponce' ]
-});
+  // Convertir le temps en secondes
+  const totalSecondes = heures * 3600 + minutes * 60 + secondes;
 
-client.connect();
+  // Multiplier par le multiplicateur
+  const tempsMultiplie = totalSecondes * multiplicateur;
 
-client.on('cheer', (channel, userstate) => {
-  console.log(channel["display-name"]);
-  console.log(userstate.bits);
-  console.log('ici')
-  notificationMessage.innerHTML = `${userstate.bits} bits offerts par ${channel["display-name"]}`
-  notification.style.transform = 'translateY(0)';
-  notification.style.opacity = '1';
-  setTimeout(() => {
-    notification.style.transform = 'translateY(50%)';
-    notification.style.opacity = '0';
-  }, 5000);
-});
+  // Convertir le temps résultant en format "hh:mm:ss"
+  const heuresResultat = Math.floor(tempsMultiplie / 3600);
+  const minutesResultat = Math.floor((tempsMultiplie % 3600) / 60);
+  const secondesResultat = tempsMultiplie % 60;
+
+  // Formater le résultat avec deux chiffres pour chaque partie (exemple: "02:03:05")
+  const tempsFormate = `${heurePad(heuresResultat)}:${heurePad(minutesResultat)}:${heurePad(secondesResultat)}`;
+
+  return tempsFormate;
+}
+
+// Fonction pour ajouter un zéro devant les chiffres inférieurs à 10
+function heurePad(valeur) {
+  return valeur < 10 ? '0' + valeur : valeur;
+}
 
 // Rajoute un zéro avant le nombre si il est inférieur à 10
 const addZero = (value: number | string) => {
@@ -55,7 +61,53 @@ const addZero = (value: number | string) => {
     return `0${value}`;
   }
   return value;
+
 };
+
+const removeLeadingZero = (input) => {
+
+  const firstDigit = input[0];
+  const secondDigit = input[1];
+
+  if (firstDigit !== "0") {
+    return input;
+  } else {
+    return secondDigit ;
+  }
+}
+
+import tmi from 'tmi.js';
+
+const client = new tmi.Client({
+	channels: [ 'batsave' ]
+});
+
+client.connect();
+
+client.on('cheer', (_channel, userstate) => {
+  let addedTime = calcBits(tempsInitial, userstate.bits).split(':');
+  hours = (parseInt(hours) + parseInt(removeLeadingZero(addedTime[0]))).toString();
+  minutes = (parseInt(minutes) + parseInt(removeLeadingZero(addedTime[1]))).toString();
+  if (parseInt(minutes) > 59) {
+    minutes = (parseInt(minutes) - 60).toString();
+    hours = (parseInt(hours) + 1).toString();
+  }
+  seconds = (parseInt(seconds) + parseInt(removeLeadingZero(addedTime[2]))).toString();
+  if (parseInt(seconds) > 59) {
+    seconds = (parseInt(seconds) - 60).toString();
+    minutes = (parseInt(minutes) + 1).toString();
+  }
+  notificationMessage.innerHTML = `${userstate.bits} bits offerts par ${userstate["display-name"]}`
+  notification.style.transform = 'translateY(0)';
+  notification.style.opacity = '1';
+  setTimeout(() => {
+    notificationMessage.innerHTML = `+ ${calcBits(tempsInitial, userstate.bits)}`
+  }, 3000)
+  setTimeout(() => {
+    notification.style.transform = 'translateY(50%)';
+    notification.style.opacity = '0';
+  }, 7000);
+});
 
 // Function qui décrémente le temps et qui change les autres valeurs en fonction des conditions présentes dans la fonction
 const timer = () => {
@@ -68,17 +120,14 @@ const timer = () => {
     minutes = '59';
     hours = (parseInt(hours) - 1).toString();
   }
-  hours = addZero(hours);
-  minutes = addZero(minutes);
-  seconds = addZero(seconds);
 };
 
 // Function qui affiche le temps et qui le stop quand on arrive à 0
 const showTime = () => {
   timer();
-  hoursShow.innerHTML = hours;
-  minutesShow.innerHTML = minutes;
-  secondsShow.innerHTML = seconds;
+  hoursShow.innerHTML = addZero(hours);
+  minutesShow.innerHTML = addZero(minutes);
+  secondsShow.innerHTML = addZero(seconds);
   localStorage.setItem('hours', hours);
   localStorage.setItem('minutes', minutes);
   localStorage.setItem('seconds', seconds);
